63610d6d5c6cadc935818b1defedb1aac9c5d244
Fiona McDonald <fiona.m.mcdonald@gmail.com>
2014-08-04 17:06:30 +0100

    added validation to basic member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,5 @@
 class Member < ActiveRecord::Base
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :membership_number, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe Member, :type => :model do
 
   describe 'validation' do
     it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_numericality_of(:membership_number).only_integer }
+    it { should validate_numericality_of(:membership_number).is_greater_than(0) }
   end
 end
